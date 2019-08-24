c3280979fd8e26510b4aa151ca4094fc797cf516
lsm0221 <345088859@qq.com>
2019-08-24 19:51:36 +0800

    'lesson-5'

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,85 @@
 <template>
   <div id="app">
     <!-- mutations -->
-    <button @click="decrementCount({amount:2})">-</button>
+    <button @click="decrementCountAsync({amout:2})">-</button>
     <span>{{count}}</span>
-    <button @click="incrementCount">+</button>
+    <button @click="incrementCountAsync">+</button>
+
+    <hr />
+    <p>{{completedTodos}}</p>
+    <button @click="fetchDataAsync">FetchData</button>
   </div>
 </template>
 
 <script>
 import Count from "./components/Count";
-import { mapState, mapGetters, mapMutations } from "vuex";
-
+import { mapState, mapGetters, mapMutations, mapActions } from "vuex";
 export default {
   name: "app",
-  components: { Count },
+  components: {
+    Count
+  },
   computed: mapGetters([
     "count",
     "completedTodos",
     "completedTodosCount",
     "getTodosById"
   ]),
-  methods: mapMutations(["incrementCount", "decrementCount"])
+  methods: mapActions([
+    "incrementCountAsync",
+    "decrementCountAsync",
+    "fetchDataAsync"
+  ])
+
   // methods: {
-  //   increment() {
-  //     // 调用mutations 里的 incrementCount  第一个参数是store.js中的名字,第二个参数是值
-  //     this.$store.commit("incrementCount");
+  //   incrementCount() {
+  //     // 触发actions
+  //     this.$store.dispatch("incrementCountAsync");
   //   },
-  //   decrement(n) {
-  //     // 调用mutations 里的 decrenmentCount
-  //     this.$store.commit("decrementCount", n);
+  //   decrementCount(n) {
+  //     // 触发actions
+  //     this.$store.dispatch("decrementCountAsync", n);
+  //   },
+  //   fetchData() {
+  //     // 触发actions
+  //     this.$store.dispatch("fetchDataAsync");
+  //   }
+  // }
+
+  // computed: {
+  //   count() {
+  //     return this.$store.getters.getCount;
+  //   },
+  //   completedTodos() {
+  //     return this.$store.getters.completedTodos;
+  //   },
+  //   completedTodosCount() {
+  //     return this.$store.getters.completedTodosCount;
+  //   },
+  //   getTodosById() {
+  //     return this.$store.getters.getTodosById;
+  //   }
+  // }
+
+  // 写法3
+  // computed: {
+  //   ...mapState(["count", "todos"]),
+  //   other() {}
+  // }
+
+  // 写法2
+  // computed: mapState({
+  //   count: state => state.count,
+  //   todos: state => state.todos
+  // })
+
+  // 写法1
+  // computed: {
+  //   count() {
+  //     return this.$store.state.count;
+  //   },
+  //   todos() {
+  //     return this.$store.state.todos;
   //   }
   // }
 };
--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Axios from 'axios';
 
 Vue.use(Vuex)
 
@@ -10,50 +11,78 @@ export default new Vuex.Store({
     todos: [{
         id: 1,
         title: "todo item 1",
-        completed: true
+        completed: false
       },
       {
         id: 2,
         title: "todo item 2",
-        completed: false
+        completed: true
       },
       {
         id: 3,
         title: "todo item 3",
         completed: true
       }
-
-
     ]
   },
-  getters: { // 不会更改状态,会在状态上添加修饰
+  getters: {
     count: state => state.count,
     completedTodos: state => state.todos.filter(todo => todo.completed),
-    // completedTodos: function (state) {
-    //   return state.todos.filter(function (todo) {
-    //     return todo.completed;
-    //   })
-    // }
     completedTodosCount: (state, getters) => getters.completedTodos.length,
-    // completedTodosCount: function (state, getters) {
-    //   return getters.completedTodos, length;
-    // }
-    getTodosById: state => id => state.todos.find(todo => todo.id == id),
+    getTodosById: state => id => state.todos.find(todo => todo.id == id)
     // getTodosById: function (state) {
     //   // 处理东西
-    //   (function (id) {
+    //   (function(id) {
     //     return state.todos.find(function (todo) {
     //       return todo.id == id;
     //     })
     //   })(id)
     // }
 
+    // completedTodosCount: function (state, getters) {
+    //   return getters.completedTodos.length;
+    // }
+
+    // completedTodos: function (state) {
+    //   return state.todos.fileter(function (todo) {
+    //     return todo.completed;
+    //   })
+    // }
+
+    // count(state) {
+    //   return ++state.count;
+    // }
   },
   mutations: {
     incrementCount: state => state.count++,
-    decrementCount: (state, payload) => state.count -= payload.amount
+    decrementCount: (state, payload) => state.count -= payload.amout,
+    setTodos: (state, todos) => state.todos = todos
   },
   actions: {
-
+    incrementCountAsync: ({
+      commit
+    }) => {
+      setTimeout(() => {
+        // 解构
+        // const { commit } = context.commit
+        // const object = {
+        //   name: "lgw",
+        //   age: 21
+        // }
+        // const { name, age } = object;
+        // context/* = this.$store */.
+        commit("incrementCount");
+      }, 2000)
+    },
+    decrementCountAsync: (context, payload) => {
+      setTimeout(() => {
+        context /* = this.$store */ .commit("decrementCount", payload);
+      }, 1000)
+    },
+    async fetchDataAsync(context) {
+      const response = await Axios.get("http://jsonplaceholder.typicode.com/todos");
+      // console.log(response);
+      context.commit("setTodos", response.data);
+    }
   }
 })
