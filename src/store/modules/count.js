af50148988329524d2f616621f0ff15dc65672a5
lsm0221 <345088859@qq.com>
2019-08-25 08:48:49 +0800

    'lesson-6'

---
+++ src/store/modules/count.js
@@ -0,0 +1,39 @@
+const state = {
+    count: 0
+};
+const getters = {
+    count: state => state.count
+};
+const mutations = {
+    incrementCount: state => state.count++,
+    decrementCount: (state, payload) => state.count -= payload.amout
+};
+const actions = {
+    incrementCountAsync: ({
+        commit
+    }) => {
+        setTimeout(() => {
+            // 解构
+            // const { commit } = context.commit
+            // const object = {
+            //   name: "lgw",
+            //   age: 21
+            // }
+            // const { name, age } = object;
+            // context/* = this.$store */.
+            commit("incrementCount");
+        }, 2000)
+    },
+    decrementCountAsync: (context, payload) => {
+        setTimeout(() => {
+            context /* = this.$store */ .commit("decrementCount", payload);
+        }, 1000)
+    },
+};
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions
+}
